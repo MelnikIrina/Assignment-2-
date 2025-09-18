4c21c76a2469c315f70b4c927d576d7b5fa85a4b
MelnikIrina <22fmif.i.melnik@std.npu.edu.ua>
2025-09-19 01:51:50 +0300

    Перший варіант логіки

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-user_input = input("Введи свій варіант: ") 
+from random import randint
 
-if user_input.isdigit():
-# Якщо рядок складається тільки з цифр, сміливо перетворюємо його на число
+secret_number = randint(1,50)
 
-uess = int(user_input)
-# Тут буде ваша логіка порівняння числа (if/elif/else)
-if guess < secret_number: 
-print("Моє число більше!") 
-# і так далі...
-else:
-# Якщо користувач ввів текст, виводимо попередження 
-print("Помилка! Потрібно вводити тільки цілі числа.") 
+while True:
+        user_input = input("Введи свій варіант: ")
+
+        if user_input.isdigit():  # перевірка, чи це число
+            guess = int(user_input)
+
+            if guess < secret_number:
+                print("число більше!")
+            elif guess > secret_number:
+                print("число менше!")
+            else:
+                print(f"Молодець,! Ти вгадав число {secret_number}")
+                break
+        else:
+            print("Випив ? - За гру не сідай. Пиши тільки цифри)")
