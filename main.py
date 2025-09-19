440a73e274190e278844b7118918ba7fc1018a34
MelnikIrina <22fmif.i.melnik@std.npu.edu.ua>
2025-09-19 03:27:28 +0300

    Перший варіант логіки
    Signed-off-by: MelnikIrina <22fmif.i.melnik@std.npu.edu.ua>

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-from random import randint
-
-nickname = input("Ти увімкнув гру <Вгадай число>. Тож тепер придумай собі ім'я: ")
-print(f"Супер, {nickname}! Я загадав число від 1 до 50. Спробуй вгадати!")
-
-secret_number = randint(1,50)
-a = 0  # лічильник спроб
-game = True
-while game:
-        user_input = input("Введи свій варіант: ")
-
-        if user_input.isdigit():  # перевірка, чи це число
-            guess = int(user_input)
-            a += 1
-
-            if guess < secret_number:
+user_input = input("Введи свій варіант: ")
+if user_input.isdigit():
+# Якщо рядок складається тільки з цифр, сміливо
+перетворюємо його на число
+guess = int(user_input)
+# Тут буде ваша логіка порівняння числа (if/elif/else)
+if guess < secret_number:
                 print("- число більше!")
             elif guess > secret_number:
                 print("- число менше!")
             else:
+                print(f"Молодець,! Ти вгадав число {secret_number}")
 
-                print(f"Молодець,! Ти вгадав число {secret_number} з {a} спроби")
-                print(f"{nickname}, чи хотів би ти зіграти ще?")
-                answer = input("Y/N): ").lower()
-                if answer == "n":
-                    game = False
-        else:
-            print("Випив ? - За гру не сідай. Пиши тільки цифри)")
-
-print(f"{nickname}, дякую за гру!")
+print("Помилка! Потрібно вводити тільки цілі числа.")
