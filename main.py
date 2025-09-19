70d3c766a23581e7b136f45802db799e956dfd05
MelnikIrina <22fmif.i.melnik@std.npu.edu.ua>
2025-09-19 03:29:15 +0300

    Третій варіант, додала перезапуск
    Signed-off-by: MelnikIrina <22fmif.i.melnik@std.npu.edu.ua>

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ nickname = input("Ти увімкнув гру <Вгадай число>. Тож
 print(f"Супер, {nickname}! Я загадав число від 1 до 50. Спробуй вгадати!")
 
 secret_number = randint(1,50)
-
+a = 0  # лічильник спроб
 game = True
 while game:
         user_input = input("Введи свій варіант: ")
 
         if user_input.isdigit():  # перевірка, чи це число
             guess = int(user_input)
+            a += 1
 
             if guess < secret_number:
                 print("- число більше!")
             elif guess > secret_number:
                 print("- число менше!")
             else:
-                print(f"Молодець,! Ти вгадав число {secret_number}")
+                print(f"Молодець,! Ти вгадав число {secret_number} з {a} спроби")
+                print(f"{nickname}, чи хотів би ти зіграти ще?")
+                answer = input("Y/N): ").lower()
+                if answer == "n":
+                    game = False
         else:
             print("Випив ? - За гру не сідай. Пиши тільки цифри)")
 print(f"{nickname}, дякую за гру!")
